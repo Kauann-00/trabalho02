ea35e13042854438c461b642681e44c4fb13adae
Kauann-00 <marcoskauan2007@gmail.com>
2025-05-12 23:11:17 -0300

    Add files via upload

---
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -0,0 +1,24 @@
+// Questão 4: Estrutura de sequencia
+
+package com.mycompany.mavenproject3;
+
+import java.util.Scanner;
+
+public class Mavenproject3 {
+
+
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite a base: ");
+            double base = scanner.nextDouble();
+            
+            System.out.print("Digite o expoente: ");
+            double expoente = scanner.nextDouble();
+            
+            double resultado = Math.pow(base, expoente);
+            
+            System.out.println(base + " elevado a " + expoente + " = " + resultado);
+        }
+    }
+}
